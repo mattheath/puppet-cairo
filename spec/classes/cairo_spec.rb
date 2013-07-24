6f67182c8c96866d0383b216b1e0cf85eeba3701
Matt Heath <matt@mattheath.com>
2013-07-24 12:33:41 +0100

    Fix up specs with repository dependency and base facts

--- spec/classes/cairo_spec.rb
+++ spec/classes/cairo_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe 'cairo' do
+
+  let(:facts) do
+    {
+      :boxen_home => '/opt/boxen'
+    }
+  end
+
   it { should contain_class('cairo') }
   it { should contain_package('cairo') }
 end
